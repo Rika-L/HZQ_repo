0c38844997c461d290667dea6e2687ae9f56679c
Rika-L <2209393334@qq.com>
2025-02-12 18:24:51 +0800

    feat: monorepo配置

---
+++ packages/backend/src/index.ts
@@ -0,0 +1,15 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+const port = 7452
+console.log(`Server is running on http://localhost:${port}`)
+
+serve({
+  fetch: app.fetch,
+  port,
+})
